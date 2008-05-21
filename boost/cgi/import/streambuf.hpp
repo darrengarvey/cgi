5c3c0485842258e73f5570da3e80260843b4459b
drrngrvy <drrngrvy@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-05-21 16:46:33 +0000

    2of2: fixing corrupted repository.
    git-svn-id: https://svn.boost.org/svn/boost/sandbox/SOC/2007/cgi/trunk@45614 b8fc166d-592f-0410-95f2-cb63ce0dd405

---
+++ boost/cgi/import/streambuf.hpp
@@ -0,0 +1,25 @@
+//                     -- streambuf.hpp --
+//
+//            Copyright (c) Darren Garvey 2007.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+////////////////////////////////////////////////////////////////
+#ifndef CGI_STREAMBUF_HPP_INCLUDED__
+#define CGI_STREAMBUF_HPP_INCLUDED__
+
+#include <boost/asio/streambuf.hpp>
+
+namespace cgi {
+ namespace common {
+
+  using boost::asio::streambuf;
+
+ } // namespace common
+
+   using common::streambuf;
+
+} // namespace cgi
+
+#endif // CGI_STREAMBUF_HPP_INCLUDED__
---
+++ boost/cgi/import/write.hpp
@@ -0,0 +1,23 @@
+//                     -- write.hpp --
+//
+//            Copyright (c) Darren Garvey 2007.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+////////////////////////////////////////////////////////////////
+#ifndef CGI_WRITE_HPP_INCLUDED
+#define CGI_WRITE_HPP_INCLUDED
+
+#include <boost/asio/write.hpp>
+
+namespace cgi {
+ namespace common {
+
+  using boost::asio::write;
+  using boost::asio::async_write;
+
+ } // namespace common
+} // namespace cgi
+
+#endif // CGI_WRITE_HPP_INCLUDED
