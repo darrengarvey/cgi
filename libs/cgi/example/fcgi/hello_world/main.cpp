d5731d652b34e58f8cc5084e13935600f05a44e4
drrngrvy <drrngrvy@b8fc166d-592f-0410-95f2-cb63ce0dd405>
2008-06-02 23:45:59 +0000

    Reverted fcgi hello_world example.
    git-svn-id: https://svn.boost.org/svn/boost/sandbox/SOC/2007/cgi/trunk@46067 b8fc166d-592f-0410-95f2-cb63ce0dd405

--- libs/cgi/example/fcgi/hello_world/main.cpp
+++ libs/cgi/example/fcgi/hello_world/main.cpp
@@ -12,52 +12,21 @@
 // The simplest FastCGI program, outputs only "Hello there, universe."
 //
 
-#include <iomanip>
-#include <fstream>
 #include <boost/cgi/fcgi.hpp>
 #include <boost/cgi.hpp>
-#include <boost/asio.hpp>
-#include <sys/socket.h>
 
 using namespace std;
 using namespace boost::fcgi;
 
-template<typename Request, typename Response>
-int handle_request(Request& req, Response& resp)
-{
-  // This is a minimal response. The content_type(...) may go before or after
-  // the response text.
-  resp<< content_type("text/plain")
-      << "Hello there, universe.";
-
-  return_(resp, req, 0);
-}
-
-/// Handle a vanilla CGI request
-int handle_request()
-{
-  boost::cgi::request req;
-  response resp;
-  return handle_request(req,resp);
-}
-
+=======
 /// Handle a FastCGI request
 template<typename Acceptor>
 int handle_request(Acceptor& a)
 {
-  request req(a.protocol_service());   // Our request.
-
   int ret = 0;
   for (;;) // Handle requests until something goes wrong
            // (an exception will be thrown).
-  {
-    a.accept(req);
-    response resp;    // A response object to make our lives easier.
-    ret = handle_request(req, resp);
-  }
-
-  return ret;
-}
+>>>>>>> .r46066
 
 int main()
 {
@@ -65,24 +34,23 @@ try
 {
   service s;        // This becomes useful with async operations.
   acceptor a(s);
-  ofstream file("/var/www/log/is_cgi.log", std::ios::app);
-
-  if (a.is_cgi())
-  {
-    file<< "Is CGI" << endl;
-    file.flush();
-    handle_request();
-  }
-  else
-  {
-    file<< "Is FastCGI (probably)";
-    file.flush();
-    handle_request(a);
-  }
-
-  file.flush();
   
-  return 0;
+  for (;;)
+  {
+    request req(s);   // Our request.
+  
+    for (;;) // Handle requests until something goes wrong
+             // (an exception will be thrown).
+    {
+      a.accept(req);
+      response resp;    // A response object to make our lives easier.
+      ret = handle_request(req, resp);
+      if (ret) break; // Use a new request if something went wrong.
+    }
+    if (!a.is_open()) break; // Quit completely if the acceptor bails out.
+  }
+
+  return ret;
 }
 catch(boost::system::system_error& err)
 {
