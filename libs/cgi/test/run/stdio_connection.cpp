d504b5e9d743a8b59b804659793ddc3cd8db1592
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2019-04-12 15:33:58 +0200

    test: stdio_connection: make fix compilation
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- libs/cgi/test/run/stdio_connection.cpp
+++ libs/cgi/test/run/stdio_connection.cpp
@@ -1,14 +1,13 @@
 #include <boost/system/error_code.hpp>
-#include "boost/cgi/buffer.hpp"
+#include "boost/cgi/import/buffer.hpp"
 #include "boost/cgi/connections/stdio.hpp"
 
 int main()
 {
-  boost::cgi::common::connections::stdio conn;
-
+  boost::cgi::connections::stdio conn;
   boost::system::error_code ec;
 
-  conn.write_some(cgi::buffer("Hello, world"), ec);
+  conn.write_some(boost::cgi::common::buffer("Hello, world"), ec);
 
   return 0;
 }
